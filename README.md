d05f32fde3c327030e9a8235c5709fc87771026e
kill055 <twitch.kill032@gmail.com>
2026-03-28 12:52:27 +0100

    v1.0.20

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Battly Launcher
+<p align="center"><img src="./src/assets/images/icon_transparent.png.png" width="150px" height="150px" alt="aventium softworks"></p>
+
+<h1 align="center">Battly Launcher</h1>
+
+<em><h5 align="center">( Battly Studios )</h5></em>
--- build.js
+++ build.js
@@ -26,7 +26,7 @@ async function obfuscateSrc() {
         const jsFiles = files.filter(file => path.extname(file) === '.js');
 
         // Obfuscar cada archivo .js
-        await Promise.all(jsFiles.map(file => obfuscateFile(path.join(srcPath, file))));
+        //await Promise.all(jsFiles.map(file => obfuscateFile(path.join(srcPath, file))));
     } catch (error) {
         console.error('Error al obfuscar el código', error);
     }
@@ -52,17 +52,18 @@ obfuscateSrc().then(() => {
             directories: { "output": "dist" },
             compression: 'maximum',
             asar: false,
-            //asar unpack de 7zip-bin
-            asarUnpack: [
-                "node_modules/7zip-bin/**/*",
-                "node_modules/7zip/**/*",
-            ],
             win: {
                 icon: "./src/assets/images/icon.ico",
-                target: [{
-                    target: "nsis",
-                    arch: ["x64", "ia32"]
-                }]
+                target: [
+                    {
+                        target: "nsis",
+                        arch: ["x64", "ia32"]
+                    },
+                    // {
+                    //     target: "appx",
+                    //     arch: ["x64", "arm64"]
+                    // }
+                ]
             },
             nsis: {
                 oneClick: false,
@@ -84,37 +85,38 @@ obfuscateSrc().then(() => {
             },
             linux: {
                 icon: "./src/assets/images/icon.png",
-                target: [{
-                    target: "AppImage",
-                    arch: ["x64"]
-                }, {
-                    target: "tar.gz",
-                    arch: ["x64"]
-                },
-                {
-                    target: "deb",
-                    arch: ["x64"]
-                },
-                {
-                    target: "rpm",
-                    arch: ["x64"]
-                },
-                {
-                    target: "AppImage",
-                    arch: ["armv7l"]
-                },
-                {
-                    target: "tar.gz",
-                    arch: ["armv7l"]
-                },
-                {
-                    target: "deb",
-                    arch: ["armv7l"]
-                },
-                {
-                    target: "rpm",
-                    arch: ["armv7l"]
-                }
+                target: [
+                    {
+                        target: "AppImage",
+                        arch: ["x64"]
+                    }, {
+                        target: "tar.gz",
+                        arch: ["x64"]
+                    },
+                    {
+                        target: "deb",
+                        arch: ["x64"]
+                    },
+                    {
+                        target: "rpm",
+                        arch: ["x64"]
+                    },
+                    {
+                        target: "AppImage",
+                        arch: ["armv7l"]
+                    },
+                    {
+                        target: "tar.gz",
+                        arch: ["armv7l"]
+                    },
+                    {
+                        target: "deb",
+                        arch: ["armv7l"]
+                    },
+                    {
+                        target: "rpm",
+                        arch: ["armv7l"]
+                    }
                 ]
             }
         }
